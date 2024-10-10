f25bf13bde808ffc8fbb83658b6dfd9724df2e52
Samir Ibrahim <84662823+sidev86@users.noreply.github.com>
2024-10-10 10:52:03 +0200

    Put main functs for testing

--- get_next_line.c
+++ get_next_line.c
@@ -88,3 +88,45 @@ char	*get_next_line(int fd)
 	str = 0;
 	return (NULL);
 }
+
+// 'VERSION 1' MAIN TEST (READ 1 LINE OF THE FILE)
+  /*int main(int argc, char **argv) 
+  {
+    int fd = open("filename.txt", O_RDONLY);
+    if (fd < 0) 
+    {
+        perror("Error opening file");
+        return 1;
+    }
+    char *line = get_next_line(fd);
+    if (line != NULL) 
+    {
+        printf("%s", line);
+        free(line);
+    }
+    close(fd);
+    return 0;
+}*/
+
+
+
+// 'VERSION 2' MAIN TEST (READ ALL THE LINES IN THE FILE)
+  /*int main(int argc, char **argv) 
+  {
+    int fd = open("filename.txt", O_RDONLY);
+    if (fd < 0) 
+    {
+        perror("Error opening file");
+        return 1;
+    }
+    char *line;
+    while ((line = get_next_line(fd)) != NULL) 
+    {
+        printf("%s", line);
+        free(line);
+    }
+    close(fd);
+    return 0;
+}*/
+
+
